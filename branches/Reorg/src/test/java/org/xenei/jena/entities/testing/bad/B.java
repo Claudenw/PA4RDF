aff7de174cfbb71c79ff1b92fa44af959d256d79
claudenw <claudenw@cbf1dfa0-5e38-4fc8-b55c-6d5501dc74cf>
2013-05-18 06:54:08 +0000

    Initial checkin of branch NOT FUNCTIONAL

---
+++ branches/Reorg/src/test/java/org/xenei/jena/entities/testing/bad/B.java
@@ -0,0 +1,24 @@
+package org.xenei.jena.entities.bad;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+import org.xenei.jena.entities.annotations.Predicate;
+import org.xenei.jena.entities.annotations.Subject;
+
+/**
+ * A class that will fail.
+ * 
+ * This should fail because getModel() does not have an implementation and this does not extend
+ * Resource
+ */
+@Subject( namespace = "http://example.com/" )
+public interface B
+{
+	Model getModel();
+
+	String getX();
+
+	@Predicate
+	void setX( String x );
+
+}
