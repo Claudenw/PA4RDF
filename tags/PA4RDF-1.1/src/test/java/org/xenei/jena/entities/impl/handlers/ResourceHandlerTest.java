20deb959b0465d9a494c6e723d31fadb2791eeb8
claudenw <claudenw@cbf1dfa0-5e38-4fc8-b55c-6d5501dc74cf>
2013-07-10 17:17:53 +0000

    [maven-release-plugin]  copy for tag PA4RDF-1.1

---
+++ tags/PA4RDF-1.1/src/test/java/org/xenei/jena/entities/impl/handlers/ResourceHandlerTest.java
@@ -0,0 +1,60 @@
+package org.xenei.jena.entities.impl.handlers;
+
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResourceHandlerTest implements HandlerTestInterface
+{
+	ResourceHandler handler;
+	RDFNode node;
+	Integer instance;
+
+	@Before
+	public void setup()
+	{
+		handler = new ResourceHandler();
+		node = ResourceFactory.createResource();
+		instance = 5;
+	}
+
+	@Override
+	@Test
+	public void testCreateRDFNode()
+	{
+		final RDFNode n = handler.createRDFNode(node);
+		Assert.assertNotNull(n);
+		Assert.assertEquals(node, n);
+	}
+
+	@Override
+	@Test
+	public void testIsEmpty()
+	{
+		Assert.assertTrue(handler.isEmpty(null));
+		Assert.assertFalse(handler.isEmpty(node));
+		Assert.assertTrue(handler.isEmpty("foo"));
+		Assert.assertTrue(handler.isEmpty(""));
+		Assert.assertTrue(handler.isEmpty(" "));
+		Assert.assertTrue(handler.isEmpty(ResourceFactory.createResource("")));
+		Assert.assertTrue(handler.isEmpty(ResourceFactory.createResource(" ")));
+		Assert.assertFalse(handler.isEmpty(ResourceFactory.createResource()));
+		Assert.assertTrue(handler.isEmpty(ResourceFactory.createTypedLiteral(
+				"", XSDDatatype.XSDstring)));
+		Assert.assertTrue(handler.isEmpty(ResourceFactory.createTypedLiteral(
+				" ", XSDDatatype.XSDstring)));
+	}
+
+	@Override
+	@Test
+	public void testParseObject()
+	{
+		final Object o = handler.parseObject(node);
+		Assert.assertNotNull(o);
+		Assert.assertEquals(node, o);
+	}
+}
