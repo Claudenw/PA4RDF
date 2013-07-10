ab780b30214d4d239ee24d2beae25d035b62aa8e
claudenw <claudenw@cbf1dfa0-5e38-4fc8-b55c-6d5501dc74cf>
2013-07-10 16:44:25 +0000

    [maven-release-plugin]  copy for tag PA4RDF-1.1

---
+++ tags/PA4RDF-1.1/src/test/java/org/xenei/jena/entities/A.java
@@ -0,0 +1,21 @@
+package org.xenei.jena.entities;
+
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import org.xenei.jena.entities.annotations.Predicate;
+import org.xenei.jena.entities.annotations.Subject;
+
+@Subject( namespace = "http://example.com/" )
+public interface A extends Resource
+{
+	String getX();
+
+	String getZ();
+
+	@Predicate
+	void setX( String x );
+
+	@Predicate( emptyIsNull = true )
+	void setZ( String z );
+
+}
---
+++ tags/PA4RDF-1.1/src/test/java/org/xenei/jena/entities/EntityManagerTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2012, XENEI.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.xenei.jena.entities;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import org.apache.commons.proxy.exception.InvokerException;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.xenei.jena.entities.bad.B;
+import org.xenei.jena.entities.impl.EntityManagerImpl;
+
+public class EntityManagerTest
+{
+
+	private EntityManager manager = new EntityManagerImpl();
+
+	@Before
+	public void setup()
+	{
+		PropertyConfigurator.configure("./src/test/resources/log4j.properties");
+	}
+
+	@Test
+	public void testBasicParser() throws Exception
+	{
+		manager.getSubjectInfo(MultiValueObjectTestClass.class);
+		manager.getSubjectInfo(CollectionValueObjectTestClass.class);
+	}
+
+	@Test
+	public void testClassParser() throws Exception
+	{
+		manager.parseClasses(new String[] {
+				MultiValueObjectTestClass.class.getName(),
+				CollectionValueObjectTestClass.class.getName() });
+		SubjectInfo ci = manager
+				.getSubjectInfo(MultiValueObjectTestClass.class);
+		Assert.assertNotNull(ci
+				.getPredicateInfo(MultiValueObjectTestClass.class
+						.getMethod("getU")));
+		ci = manager.getSubjectInfo(CollectionValueObjectTestClass.class);
+		Assert.assertNotNull(ci
+				.getPredicateInfo(CollectionValueObjectTestClass.class
+						.getMethod("getU")));
+	}
+
+	@Test
+	public void testPathParser() throws Exception
+	{
+		manager.parseClasses(new String[] { "org.xenei.jena.entities" });
+		SubjectInfo ci = manager
+				.getSubjectInfo(MultiValueObjectTestClass.class);
+		Assert.assertNotNull(ci
+				.getPredicateInfo(MultiValueObjectTestClass.class
+						.getMethod("getU")));
+		ci = manager.getSubjectInfo(CollectionValueObjectTestClass.class);
+		Assert.assertNotNull(ci
+				.getPredicateInfo(CollectionValueObjectTestClass.class
+						.getMethod("getU")));
+	}
+
+	@Test
+	public void testPathParserWithBadClasses() throws Exception
+	{
+		Model model = ModelFactory.createDefaultModel();
+		try
+		{
+			manager.parseClasses(new String[] { "org.xenei.jena.entities.bad" });
+			manager.read( model.createResource(), org.xenei.jena.entities.bad.A.class, B.class);
+			Assert.fail("Should have thrown InvokerException");
+		}
+		catch (final InvokerException e)
+		{
+			// expected
+		}
+	}
+}
