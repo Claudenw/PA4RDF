1fb474cda3f80f432ced9ec6df3393512a1fd651
claudenw <claudenw@cbf1dfa0-5e38-4fc8-b55c-6d5501dc74cf>
2015-02-15 20:37:31 +0000

    Merged EntityManger changes. cleaned up PredicateInfoImpl

--- src/main/java/org/xenei/jena/entities/EntityManager.java
+++ src/main/java/org/xenei/jena/entities/EntityManager.java
@@ -3,9 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,53 +17,50 @@ package org.xenei.jena.entities;
 
 import org.xenei.jena.entities.annotations.Subject;
 
-import com.hp.hpl.jena.rdf.model.Resource;
-
 /**
  * An Entity Manager to manage instances of entities annotated with the Subject
  * annotation
- * 
+ *
  * Subject Annotation
- * 
+ *
  * The EntityManager handles all client interactions with the Jena Model using
  * the annotated classes.
- * 
+ *
  * @see org.xenei.jena.entities.annotations.Subject
  */
-public interface EntityManager
-{
+public interface EntityManager {
 
 	/**
 	 * Get the subject annotation for the class.
-	 * 
-	 * Returns the Subject annotation if it exists for the class.
-	 * Searches the super classes for the subject annotation.
-	 * If no annotation is found in the super classes all the interfaces are
-	 * searched and the first interface that contains the annotation is
-	 * returned.
-	 * 
-	 * @param clazz The class to get the subject for.
+	 *
+	 * Returns the Subject annotation if it exists for the class. Searches the
+	 * super classes for the subject annotation. If no annotation is found in
+	 * the super classes all the interfaces are searched and the first interface
+	 * that contains the annotation is returned.
+	 *
+	 * @param clazz
+	 *            The class to get the subject for.
 	 * @return the Subject annotation or null if no annotation is found.
 	 */
-	public Subject getSubject( final Class<?> clazz );
+	public Subject getSubject(final Class<?> clazz);
 
 	/**
 	 * Get the SubjectInfo for the class.
-	 * 
+	 *
 	 * @param clazz
 	 *            The class to get SubjectInfo for.
 	 * @return The SubjectInfo
 	 * @throws IllegalArgumentException
 	 *             if clazz is not properly annotated with Subject annotations.
 	 */
-	public SubjectInfo getSubjectInfo( Class<?> clazz );
+	public SubjectInfo getSubjectInfo(Class<?> clazz);
 
 	/**
 	 * Determine if target has all the properties required in the Subject( type
 	 * ) annotation value.
-	 * 
+	 *
 	 * If target is not a Resource or ResourceWrapper returns false.
-	 * 
+	 *
 	 * @param target
 	 *            The object to check.
 	 * @param clazz
@@ -73,70 +70,55 @@ public interface EntityManager
 	 * @throws IllegalArgumentException
 	 *             if clazz is not a Subject annotated class.
 	 */
-	public boolean isInstance( Object target, Class<?> clazz );
-
-	
-	/**
-	 * Verify r has all the annotation specified properties of clazz.
-	 * If r does not have the
-	 * required types as defined in the Subject annotation they will be added.
-	 * 
-	 * @param r
-	 *            The Resource to verify.
-	 * @param clazz
-	 *            The Subject annotated class to verify against.
-	 * @return r for chaining
-	 */
-	public <T> T addInstanceProperties( final T r,
-			final Class<?> clazz );
+	public boolean isInstance(Object target, Class<?> clazz);
 
 	/**
 	 * Parses the the classes in a package (and subpackages) looking for Subject
 	 * annotated classes.
-	 * 
+	 *
 	 * Classes are located using classLoader.getResource(packageAsPath)
-	 * 
+	 *
 	 * @see java.lang.ClassLoader#getResources(String)
-	 * 
+	 *
 	 *      If any Subject annotated classes are missing required annotations, a
-	 *      log entry is written.
-	 *      If any Subject annotated classes faild parsing a MissingAnnotation
-	 *      exception is thrown after
-	 *      all classes have been processed.
-	 * 
+	 *      log entry is written. If any Subject annotated classes faild parsing
+	 *      a MissingAnnotation exception is thrown after all classes have been
+	 *      processed.
+	 *
 	 * @param packageName
 	 *            The name of the package to process
-	 * @throws MissingAnnotation if a subject annotated class has no predicate annotations.
+	 * @throws MissingAnnotation
+	 *             if a subject annotated class has no predicate annotations.
 	 */
-	public void parseClasses( String packageName ) throws MissingAnnotation;
+	public void parseClasses(String packageName) throws MissingAnnotation;
 
 	/**
 	 * Parses the the classes in an array of packages (and subpackages) looking
 	 * for Subject annotated classes.
-	 * 
+	 *
 	 * Classes are located using classLoader.getResource(packageAsPath)
-	 * 
-	 * 
-	 * If any Subject annotated classes are missing required annotations, a
-	 * log entry is written.
-	 * If any Subject annotated classes faild parsing a MissingAnnotation
-	 * exception is thrown after
-	 * all classes have been processed.
-	 * 
+	 *
+	 *
+	 * If any Subject annotated classes are missing required annotations, a log
+	 * entry is written. If any Subject annotated classes faild parsing a
+	 * MissingAnnotation exception is thrown after all classes have been
+	 * processed.
+	 *
 	 * @param packageNames
 	 *            The array of package names to process
-	 * @throws MissingAnnotation if a subject annotated class has not predicate annotations.
+	 * @throws MissingAnnotation
+	 *             if a subject annotated class has not predicate annotations.
 	 * @see java.lang.ClassLoader#getResources(String)
 	 */
-	public void parseClasses( String[] packageNames ) throws MissingAnnotation;
+	public void parseClasses(String[] packageNames) throws MissingAnnotation;
 
 	/**
 	 * Make an instance of clazz from source.
-	 * 
-	 * If source does not pass isInstance() check additional properties are added
-	 * as required.
-	 * 
-	 * 
+	 *
+	 * If source does not pass isInstance() check additional properties are
+	 * added as required.
+	 *
+	 *
 	 * @see #isInstance(Object, Class)
 	 * @param source
 	 *            Must either implement Resource or ResourceWrapper interfaces.
@@ -144,21 +126,22 @@ public interface EntityManager
 	 *            The class of the object to be returned.
 	 * @param secondaryClasses
 	 *            A lost of other classes that are implemented.
+	 * @param <T>
+	 *            the instance type to return.
 	 * @return primaryClass instance that also implements ResourceWrapper.
 	 * @throws MissingAnnotation
 	 *             if any of the classes do not have Subject annotations.
 	 * @throws IllegalArgumentException
 	 *             if source implements neither Resource nor ResourceWrapper.
 	 */
-	public <T> T make( Object source, Class<T> primaryClass,
-			Class<?>... secondaryClasses ) throws MissingAnnotation;
+	public <T> T make(Object source, Class<T> primaryClass,
+			Class<?>... secondaryClasses) throws MissingAnnotation;
 
-	
 	/**
 	 * Read an instance of clazz from source.
-	 * 
+	 *
 	 * Does not verify that the resource passes the isInstance() check.
-	 * 
+	 *
 	 * @see #isInstance(Object, Class)
 	 * @param source
 	 *            Must either implement Resource or ResourceWrapper interfaces.
@@ -166,47 +149,52 @@ public interface EntityManager
 	 *            The class of the object to be returned.
 	 * @param secondaryClasses
 	 *            A lost of other classes that are implemented.
-	 * @param <T> the instance type to return.
+	 * @param <T>
+	 *            the instance type to return.
 	 * @return primaryClass instance that also implements ResourceWrapper.
 	 * @throws MissingAnnotation
 	 *             if any of the classes do not have Subject annotations.
 	 * @throws IllegalArgumentException
 	 *             if source implements neither Resource nor ResourceWrapper.
 	 */
-	public <T> T read( Object source, Class<T> primaryClass,
-			Class<?>... secondaryClasses ) throws MissingAnnotation, IllegalArgumentException;
+	public <T> T read(Object source, Class<T> primaryClass,
+			Class<?>... secondaryClasses) throws MissingAnnotation,
+			IllegalArgumentException;
 
 	/**
 	 * Read an instance of clazz from Object source. If source does not have the
-	 * required types as defined in the Subject annotation of clazz they will be added.
+	 * required types as defined in the Subject annotation of clazz they will be
+	 * added.
 	 * <p>
-	 * This method may modify the graph and should be called within the scope of a transaction
-	 * if the underlying graph requires them.
+	 * This method may modify the graph and should be called within the scope of
+	 * a transaction if the underlying graph requires them.
 	 * </p>
-	 * 
+	 *
 	 * @param source
 	 *            Must either implement Resource or ResourceWrapper interfaces.
 	 * @param clazz
 	 *            The class containing the Subject annotation.
+	 * @param <T>
+	 *            the instance type to return.
 	 * @return source for chaining
 	 * @throws MissingAnnotation
 	 *             if clazz does not have Subject annotations.
 	 * @throws IllegalArgumentException
 	 *             if source implements neither Resource nor ResourceWrapper.
 	 */
-	public Resource addInstanceProperties( final Object source, final Class<?> clazz ) throws MissingAnnotation;
-			
+	public <T> T addInstanceProperties(final T source, final Class<?> clazz)
+			throws MissingAnnotation;
+
 	/**
 	 * Calls the target.setX predicate methods with the results of the
-	 * source.getX predicate
-	 * methods.
-	 * 
+	 * source.getX predicate methods.
+	 *
 	 * @param source
 	 *            The object to copy data from.
 	 * @param target
 	 *            The object to copy data to.
 	 * @return The target object for chaining.
 	 */
-	public Object update( Object source, Object target );
+	public Object update(Object source, Object target);
 
 }
